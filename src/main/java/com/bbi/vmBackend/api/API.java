a0708189584ec0cb11ac1b3b6edf3916c6b89e0f
NorhanAzim <22336655Home>
2017-12-21 19:17:33 +0200

    listNotification

--- src/main/java/com/bbi/vmBackend/api/API.java
+++ src/main/java/com/bbi/vmBackend/api/API.java
@@ -3,11 +3,8 @@ package com.bbi.vmBackend.api;
 import java.util.Date;
 
 import javax.ws.rs.GET;
-<<<<<<< HEAD
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
-=======
->>>>>>> 8714dcd02e18f9f20df84fc5ce879068f4d4c96b
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -21,22 +18,15 @@ import com.bbi.vmBackend.da.dao.Session;
 public class API {
 	// http://localhost:8080/VM_Syatem/rest/api/
 
-<<<<<<< HEAD
 	@POST
-=======
-	@GET
->>>>>>> 8714dcd02e18f9f20df84fc5ce879068f4d4c96b
+
 	@Path("/{userName}/{password}")
 	@Produces(MediaType.APPLICATION_JSON)
-<<<<<<< HEAD
-	public void login(@PathParam("userName") String userName, @PathParam("password") String password) {
-=======
+
 	public Response login(@PathParam("userName") String userName,
 			@PathParam("password") String password) {
->>>>>>> 9b3ddfcd03dec709d40bfd520fe166720c5888df
 		System.out.println("-----------i'm in Login-------------------");
 		Validation val = new Validation();
-<<<<<<< HEAD
 		Response response = null;
 		if (val.loginValidation(userName, password)) {
 			Authentication authentication = new Authentication();
@@ -52,29 +42,7 @@ public class API {
 			System.out.println("-------------user not valid--------------");
 		}
 		return response;
-=======
-		if (val.loginValidation(userName, password)) {
-			Authentication authentication = new Authentication();
-<<<<<<< HEAD
-			authentication.isAuthenticated(userName, password);
-=======
-			authentication.isUserAuthenticated(userName,password);
-			
-			 Response.ResponseBuilder rb = Response.ok("the test response");
-		      Response response = rb.header("header1", "value1")
-		                            .header("header2", "value2")
-		                            .build();
-			
-		}
-		else System.out.println("-------------user not valid--------------");
-		return null;
->>>>>>> 9b3ddfcd03dec709d40bfd520fe166720c5888df
->>>>>>> 8714dcd02e18f9f20df84fc5ce879068f4d4c96b
-
-		} else
-			System.out.println("-------------user not valid--------------");
 	}
-
 	@GET
 	@Path("/{user_id}/{token}/{key}/{lastInsertion}/{lastUpdate}")
 	@Produces(MediaType.APPLICATION_JSON)
