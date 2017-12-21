84eb3b53746c666b2b48f5e56045f8027c3c11b8
NorhanAzim <22336655Home>
2017-12-21 18:11:52 +0200

    Edit authentication

--- src/main/java/com/bbi/vmBackend/businessLogic/Authentication.java
+++ src/main/java/com/bbi/vmBackend/businessLogic/Authentication.java
@@ -1,6 +1,5 @@
 package com.bbi.vmBackend.businessLogic;
 
-<<<<<<< HEAD
 import com.bbi.vmBackend.da.dao.Employee;
 import com.bbi.vmBackend.da.dao.Session;
 import com.bbi.vmBackend.facade.DataAccessFacade;
@@ -15,30 +14,7 @@ public class Authentication {
 			return dataAccessFacade.facadeGetSession(employee);
 		else
 			return null;
-	}
-}
-=======
-import java.io.IOException;
-import java.util.Base64;
-import java.util.StringTokenizer;
-
-import com.bbi.vmBackend.da.EmployeeHome;
-import com.bbi.vmBackend.da.dao.Employee;
-
-public class Authentication {
-
-	public boolean isUserAuthenticated(String userName, String password) {
-		EmployeeHome employeeHome = new EmployeeHome();
-		Session session = new Session();
-		
-		Employee employee = employeeHome.getEmployee(userName, password);
-		if (employee != null) {
-			Session sessionId = session.insertSession(employee.getUserId());
-			return true;
-		} else
-			return false;
 
 	}
 
 }
->>>>>>> 0dde1115d285363a0ba96ed74d82072801761ebd
